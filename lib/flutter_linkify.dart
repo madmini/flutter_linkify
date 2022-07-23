cc4155de8beaef8ca392820f16974d4f116f9ae6
Max Jakobitsch <minigugler@gmail.com>
2022-07-23 19:43:55 +0200

    refactor: simplify (Selectable)Linkify
    extend (Selectable)Text using super parameters

--- lib/flutter_linkify.dart
+++ lib/flutter_linkify.dart
@@ -19,7 +19,7 @@ export 'package:linkify/linkify.dart'
 typedef LinkCallback = void Function(LinkableElement link);
 
 /// Turns URLs into links
-class Linkify extends StatelessWidget {
+class Linkify extends Text {
   /// Text to be linkified
   final String text;
 
@@ -34,112 +34,44 @@ class Linkify extends StatelessWidget {
 
   // TextSpan
 
-  /// Style for non-link text
-  final TextStyle? style;
-
   /// Style of link text
   final TextStyle? linkStyle;
 
-  // Text.rich
-
-  /// How the text should be aligned horizontally.
-  final TextAlign textAlign;
-
-  /// Text direction of the text
-  final TextDirection? textDirection;
-
-  /// The maximum number of lines for the text to span, wrapping if necessary
-  final int? maxLines;
-
-  /// How visual overflow should be handled.
-  final TextOverflow overflow;
-
-  /// The number of font pixels for each logical pixel
-  final double textScaleFactor;
-
-  /// Whether the text should break at soft line breaks.
-  final bool softWrap;
-
-  /// The strut style used for the vertical layout
-  final StrutStyle? strutStyle;
-
-  /// Used to select a font when the same Unicode character can
-  /// be rendered differently, depending on the locale
-  final Locale? locale;
-
-  /// Defines how to measure the width of the rendered text.
-  final TextWidthBasis textWidthBasis;
-
-  /// Defines how the paragraph will apply TextStyle.height to the ascent of the first line and descent of the last line.
-  final TextHeightBehavior? textHeightBehavior;
-
-  const Linkify({
-    Key? key,
+  Linkify({
+    super.key,
     required this.text,
     this.linkifiers = defaultLinkifiers,
     this.onOpen,
     this.options = const LinkifyOptions(),
     // TextSpan
-    this.style,
+    super.style,
     this.linkStyle,
-    // RichText
-    this.textAlign = TextAlign.start,
-    this.textDirection,
-    this.maxLines,
-    this.overflow = TextOverflow.clip,
-    this.textScaleFactor = 1.0,
-    this.softWrap = true,
-    this.strutStyle,
-    this.locale,
-    this.textWidthBasis = TextWidthBasis.parent,
-    this.textHeightBehavior,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final elements = linkify(
-      text,
-      options: options,
-      linkifiers: linkifiers,
-    );
-
-    return Text.rich(
-      buildTextSpan(
-        elements,
-        style: Theme.of(context).textTheme.bodyText2?.merge(style),
-        onOpen: onOpen,
-        useMouseRegion: true,
-        linkStyle: Theme.of(context)
-            .textTheme
-            .bodyText2
-            ?.merge(style)
-            .copyWith(
+    // SelectableText
+    super.textAlign,
+    super.textDirection,
+    super.maxLines,
+    super.textScaleFactor = 1.0,
+    super.strutStyle,
+    super.textWidthBasis,
+    super.textHeightBehavior,
+  }) : super.rich(
+          LinkifySpan(
+            text: text,
+            options: options,
+            linkifiers: linkifiers,
+            onOpen: onOpen,
+            linkStyle: const TextStyle(
               color: Colors.blueAccent,
               decoration: TextDecoration.underline,
-            )
-            .merge(linkStyle),
-      ),
-      textAlign: textAlign,
-      textDirection: textDirection,
-      maxLines: maxLines,
-      overflow: overflow,
-      textScaleFactor: textScaleFactor,
-      softWrap: softWrap,
-      strutStyle: strutStyle,
-      locale: locale,
-      textWidthBasis: textWidthBasis,
-      textHeightBehavior: textHeightBehavior,
-    );
-  }
+            ).merge(linkStyle),
+          ),
+        );
 }
 
 /// Turns URLs into links
-class SelectableLinkify extends StatelessWidget {
+class SelectableLinkify extends SelectableText {
   /// Text to be linkified
   final String text;
-
-  /// The number of font pixels for each logical pixel
-  final textScaleFactor;
 
   /// Linkifiers to be used for linkify
   final List<Linkifier> linkifiers;
@@ -152,173 +84,51 @@ class SelectableLinkify extends StatelessWidget {
 
   // TextSpan
 
-  /// Style for non-link text
-  final TextStyle? style;
-
   /// Style of link text
   final TextStyle? linkStyle;
 
-  // Text.rich
-
-  /// How the text should be aligned horizontally.
-  final TextAlign? textAlign;
-
-  /// Text direction of the text
-  final TextDirection? textDirection;
-
-  /// The minimum number of lines to occupy when the content spans fewer lines.
-  final int? minLines;
-
-  /// The maximum number of lines for the text to span, wrapping if necessary
-  final int? maxLines;
-
-  /// The strut style used for the vertical layout
-  final StrutStyle? strutStyle;
-
-  /// Defines how to measure the width of the rendered text.
-  final TextWidthBasis? textWidthBasis;
-
-  // SelectableText.rich
-
-  /// Defines the focus for this widget.
-  final FocusNode? focusNode;
-
-  /// Whether to show cursor
-  final bool showCursor;
-
-  /// Whether this text field should focus itself if nothing else is already focused.
-  final bool autofocus;
-
-  /// Configuration of toolbar options
-  final ToolbarOptions? toolbarOptions;
-
-  /// How thick the cursor will be
-  final double cursorWidth;
-
-  /// How rounded the corners of the cursor should be
-  final Radius? cursorRadius;
-
-  /// The color to use when painting the cursor
-  final Color? cursorColor;
-
-  /// Determines the way that drag start behavior is handled
-  final DragStartBehavior dragStartBehavior;
-
-  /// If true, then long-pressing this TextField will select text and show the cut/copy/paste menu,
-  /// and tapping will move the text caret
-  final bool enableInteractiveSelection;
-
-  /// Called when the user taps on this selectable text (not link)
-  final GestureTapCallback? onTap;
-
-  final ScrollPhysics? scrollPhysics;
-
-  /// Defines how the paragraph will apply TextStyle.height to the ascent of the first line and descent of the last line.
-  final TextHeightBehavior? textHeightBehavior;
-
-  /// How tall the cursor will be.
-  final double? cursorHeight;
-
-  /// Optional delegate for building the text selection handles and toolbar.
-  final TextSelectionControls? selectionControls;
-
-  /// Called when the user changes the selection of text (including the cursor location).
-  final SelectionChangedCallback? onSelectionChanged;
-
-  const SelectableLinkify({
-    Key? key,
+  SelectableLinkify({
+    super.key,
     required this.text,
     this.linkifiers = defaultLinkifiers,
     this.onOpen,
     this.options = const LinkifyOptions(),
     // TextSpan
-    this.style,
+    super.style,
     this.linkStyle,
-    // RichText
-    this.textAlign,
-    this.textDirection,
-    this.minLines,
-    this.maxLines,
     // SelectableText
-    this.focusNode,
-    this.textScaleFactor = 1.0,
-    this.strutStyle,
-    this.showCursor = false,
-    this.autofocus = false,
-    this.toolbarOptions,
-    this.cursorWidth = 2.0,
-    this.cursorRadius,
-    this.cursorColor,
-    this.dragStartBehavior = DragStartBehavior.start,
-    this.enableInteractiveSelection = true,
-    this.onTap,
-    this.scrollPhysics,
-    this.textWidthBasis,
-    this.textHeightBehavior,
-    this.cursorHeight,
-    this.selectionControls,
-    this.onSelectionChanged,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final elements = linkify(
-      text,
-      options: options,
-      linkifiers: linkifiers,
-    );
-
-    return SelectableText.rich(
-      buildTextSpan(
-        elements,
-        style: Theme.of(context).textTheme.bodyText2?.merge(style),
-        onOpen: onOpen,
-        linkStyle: Theme.of(context)
-            .textTheme
-            .bodyText2
-            ?.merge(style)
-            .copyWith(
+    super.textAlign,
+    super.textDirection,
+    super.minLines,
+    super.maxLines,
+    super.focusNode,
+    super.textScaleFactor = 1.0,
+    super.strutStyle,
+    super.showCursor = false,
+    super.autofocus = false,
+    super.toolbarOptions,
+    super.cursorWidth = 2.0,
+    super.cursorRadius,
+    super.cursorColor,
+    super.dragStartBehavior = DragStartBehavior.start,
+    super.enableInteractiveSelection = true,
+    super.onTap,
+    super.scrollPhysics,
+    super.textWidthBasis,
+    super.textHeightBehavior,
+    super.cursorHeight,
+    super.selectionControls,
+    super.onSelectionChanged,
+  }) : super.rich(
+          LinkifySpan(
+            text: text,
+            options: options,
+            linkifiers: linkifiers,
+            onOpen: onOpen,
+            linkStyle: const TextStyle(
               color: Colors.blueAccent,
               decoration: TextDecoration.underline,
-            )
-            .merge(linkStyle),
-      ),
-      textAlign: textAlign,
-      textDirection: textDirection,
-      minLines: minLines,
-      maxLines: maxLines,
-      focusNode: focusNode,
-      strutStyle: strutStyle,
-      showCursor: showCursor,
-      textScaleFactor: textScaleFactor,
-      autofocus: autofocus,
-      toolbarOptions: toolbarOptions,
-      cursorWidth: cursorWidth,
-      cursorRadius: cursorRadius,
-      cursorColor: cursorColor,
-      dragStartBehavior: dragStartBehavior,
-      enableInteractiveSelection: enableInteractiveSelection,
-      onTap: onTap,
-      scrollPhysics: scrollPhysics,
-      textWidthBasis: textWidthBasis,
-      textHeightBehavior: textHeightBehavior,
-      cursorHeight: cursorHeight,
-      selectionControls: selectionControls,
-      onSelectionChanged: onSelectionChanged,
-    );
-  }
-}
-
-class LinkableSpan extends WidgetSpan {
-  LinkableSpan({
-    required MouseCursor mouseCursor,
-    required InlineSpan inlineSpan,
-  }) : super(
-          child: MouseRegion(
-            cursor: mouseCursor,
-            child: Text.rich(
-              inlineSpan,
-            ),
+            ).merge(linkStyle),
           ),
         );
 }
@@ -341,7 +151,6 @@ TextSpan buildTextSpan(
       ),
     );
 
-/// Raw TextSpan builder for more control on the RichText
 List<InlineSpan>? buildTextSpanChildren(
   List<LinkifyElement> elements, {
   TextStyle? style,
